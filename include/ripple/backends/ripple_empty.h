8a5af0c1994c8a735350acc3c9a7b767db523334
Jan Goličnik <jan@nejka.net>
2026-08-18 21:06:21 +0200

    asd

--- include/ripple/backends/ripple_empty.h
+++ include/ripple/backends/ripple_empty.h
@@ -1,72 +1,40 @@
 #ifndef RIPPLE_EMPTY_H
 #define RIPPLE_EMPTY_H
 
-#include <marrow/marrow.h>
-
 #ifdef RIPPLE_IMPLEMENTATION
 #define RIPPLE_EMPTY_IMPLEMENTATION
 #endif // RIPPLE_IMPLEMENTATION
 
-typedef void* RippleBackendRendererConfig;
-typedef struct RippleBackendWindowRenderer { void* i; } RippleBackendWindowRenderer;
-typedef void* RippleRenderData;
-typedef void* RippleImage;
+STRUCT(RippleBackendRendererConfig) {
+    u32 _empty;
+};
 
-#include "../ripple.h"
+STRUCT(RippleRenderData)  {
+    u32 _empty;
+};
+
+typedef u32 RippleImage;
 
 #ifdef RIPPLE_EMPTY_IMPLEMENTATION
 
-RippleBackendRendererConfig ripple_backend_renderer_default_config()
+void ripple_backend_renderer_initialize(RippleBackendRendererConfig config) { }
+
+void ripple_backend_render_begin(u32 width, u32 height) { }
+
+void ripple_backend_render_end(RippleRenderData render_data, RippleColor clear_color) { }
+
+void ripple_backend_render_rect(i32 x, i32 y, i32 w, i32 h, RippleColor color1, RippleColor color2, RippleColor color3, RippleColor color4, f32 radius1, f32 radius2, f32 radius3, f32 radius4) { }
+
+void ripple_backend_render_image(i32 x, i32 y, i32 w, i32 h, RippleImage image) { }
+
+void ripple_measure_text(str text, f32 font_size, i32* out_w, i32* out_h)
 {
-    return nullptr;
+    *out_w = str_len(text) * font_size;
+    *out_h = font_size;
 }
 
-void ripple_backend_renderer_initialize(RippleBackendRendererConfig config)
-{
-}
+void ripple_backend_render_text(i32 pos_x, i32 pos_y, str text, f32 font_size, RippleColor color) { }
 
-RippleBackendWindowRenderer ripple_backend_window_renderer_create(u64 id)
-{
-    return (RippleBackendWindowRenderer) { 0 };
-}
+#endif // RIPPLE_WGPU_IMPLEMENTATION
 
-RippleRenderData ripple_backend_render_begin()
-{
-    return nullptr;
-}
-
-void ripple_backend_render_window_begin(RippleBackendWindowRenderer* renderer, RippleRenderData render_data)
-{
-}
-
-void ripple_backend_render_window_end(RippleBackendWindowRenderer* window, RippleRenderData render_data, RippleColor clear_color)
-{
-}
-
-void ripple_backend_render_end(RippleRenderData render_data)
-{
-}
-
-void ripple_backend_window_present(RippleBackendWindowRenderer* renderer)
-{
-}
-
-void ripple_backend_render_rect(RippleBackendWindowRenderer* window, i32 x, i32 y, i32 w, i32 h, RippleColor color1, RippleColor color2, RippleColor color3, RippleColor color4, f32 radius1, f32 radius2, f32 radius3, f32 radius4)
-{
-}
-
-void ripple_backend_render_image(RippleBackendWindowRenderer* window, i32 x, i32 y, i32 w, i32 h, RippleImage image)
-{
-}
-
-void ripple_measure_text(s8 text, f32 font_size, i32* out_w, i32* out_h)
-{
-}
-
-void ripple_backend_render_text(RippleBackendWindowRenderer* window, i32 pos_x, i32 pos_y, s8 text, f32 font_size, RippleColor color)
-{
-}
-
-#endif // RIPPLE_EMPTY_IMPLEMENTATION
-
-#endif // RIPPLE_EMPTY_H
+#endif // RIPPLE_WGPU_H
